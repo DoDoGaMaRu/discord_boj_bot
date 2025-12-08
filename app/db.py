153ce048b952a6c29d6490fe17f00ffc3adce85f
백대환 <nm09090@naver.com>
2025-12-09 03:48:39 +0900

    feat: db 연결 코드 작성

--- app/db.py
+++ app/db.py
@@ -1,9 +1,11 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
+from models import Base
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
+
+Base.metadata.create_all(engine)
