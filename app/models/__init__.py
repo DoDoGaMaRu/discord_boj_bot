fc31647a46d4ba5823e15f82903bd93b88647131
백대환 <nm09090@naver.com>
2025-12-09 15:56:59 +0900

    feat: models 패키지 init 갱신

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,6 @@
 from .base import Base
 
-from .best_record import BestRecord
+from .record import Record
 from .guild import Guild
 from .language import Language
 from .member import Member
@@ -9,3 +9,6 @@ from .problem_format_daily import ProblemFormatDaily
 from .problem_format_weekly import ProblemFormatWeekly
 from .workbook_daily import WorkbookDaily
 from .workbook_weekly import WorkbookWeekly
+from .tag import Tag
+from .allow_tag import AllowTag
+from .deny_tag import DenyTag
